c91e6d903b1749f8558f02c37f0f2bef0465ed75
Alka, Liene (external - Service) <liene.alka@sap.com>
2020-07-08 10:44:48 +0300

    all questions finished

--- test-questions-Mediapark.playground/Contents.swift
+++ test-questions-Mediapark.playground/Contents.swift
@@ -1,4 +1,133 @@
 import Foundation
+import UIKit
+
+/**
+1. Demo code
+ We have theoretical shop object with working hours for each day. Demo object structure is provided below,
+ image if this is the data we received from BE and we have to work with it. “WorkHours” is optional because if it is that means shop is not working that day,
+ otherwise we would have some formatted date, like “10:00", “16:30”, etc…
+ 
+ Your main task is to display shops working hours sorted by weekdays and time. If several days in succession have exact same time, those days should be grouped together (Monday-Wednesday in the example below). If shop is closed then instead of time show “Closed” (Friday in the example below).
+ 
+ For example:
+    I-III                               IV                            V                              VI-VII
+ 09:00 - 21:00            10:00 - 21:00               Closed                    09:00 - 21:00
+ 
+ ***/
+ 
+ 
+struct Shop {
+ let name: String
+ let adress: String
+ let workSchedule: WorkSchedule
+
+ struct WorkSchedule {
+   let monday: WorkHours?
+   let tuesday: WorkHours?
+   let wednesday: WorkHours?
+   let thursday: WorkHours?
+   let friday: WorkHours?
+   let saturay: WorkHours?
+   let sunday: WorkHours?
+ }
+
+ struct WorkHours {
+   let from: String
+   let to: String
+ }
+}
+
+//Generate test data for shop schedule
+let shop1 = Shop(name: "Shop Name", adress: "Shop Adress 24",
+                workSchedule: Shop.WorkSchedule(monday: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                tuesday: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                wednesday: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                thursday: Shop.WorkHours(from: "10:00", to: "21:00"),
+                                                friday: nil,
+                                                saturay: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                sunday: Shop.WorkHours(from: "09:00", to: "21:00")))
+
+let shop2 = Shop(name: "Shop Name", adress: "Shop Adress 24",
+                 workSchedule: Shop.WorkSchedule(monday: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                 tuesday: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                 wednesday: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                 thursday: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                 friday: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                 saturay: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                 sunday: Shop.WorkHours(from: "09:00", to: "21:00")))
+
+let shop3 = Shop(name: "Shop Name", adress: "Shop Adress 24",
+                workSchedule: Shop.WorkSchedule(monday: Shop.WorkHours(from: "09:00", to: "20:00"),
+                                                tuesday: Shop.WorkHours(from: "09:00", to: "21:00"),
+                                                wednesday: Shop.WorkHours(from: "09:00", to: "22:00"),
+                                                thursday: Shop.WorkHours(from: "10:00", to: "21:00"),
+                                                friday: nil,
+                                                saturay: Shop.WorkHours(from: "08:00", to: "21:00"),
+                                                sunday: Shop.WorkHours(from: "07:00", to: "21:00")))
+
+var workHours: [String] = []
+
+func displayShopWorkSchedule(shop: Shop) {
+  let dayValues = ["I", "II", "III", "IV", "V", "VI", "VII"]
+  let workSchedule = shop.workSchedule
+ 
+  workHours = []
+  
+  if let mon = workSchedule.monday { addHours(hours: mon) } else { addClosed() }
+  if let tue = workSchedule.tuesday { addHours(hours: tue) } else { addClosed() }
+  if let wed = workSchedule.wednesday { addHours(hours: wed) } else { addClosed() }
+  if let thu = workSchedule.thursday { addHours(hours: thu) } else { addClosed() }
+  if let fri = workSchedule.friday { addHours(hours: fri) } else { addClosed() }
+  if let sat = workSchedule.saturay { addHours(hours: sat) } else { addClosed() }
+  if let sun = workSchedule.sunday { addHours(hours: sun) } else { addClosed() }
+  
+  var idx = 0 // store first index of identical item in workHours array
+  var lastIdx = 0 // store last index of identical item in workHours array
+  
+  for (index, hours) in workHours.enumerated() {
+    if index > 0 {
+      if workHours[idx] == workHours[index] {
+        lastIdx = index
+      } else {
+        
+        if idx < lastIdx {
+          print("\(dayValues[idx])-\(dayValues[lastIdx])    \(workHours[lastIdx])")
+        } else {
+          print("\(dayValues[lastIdx])    \(workHours[lastIdx])")
+        }
+        
+        idx = index
+        lastIdx = index
+      }
+      
+      if index+1 == workHours.count {
+        if idx < lastIdx {
+          print("\(dayValues[idx])-\(dayValues[index])    \(hours)")
+        } else {
+          print("\(dayValues[index])    \(hours)")
+        }
+      }
+    }
+  }
+}
+
+func addHours(hours: Shop.WorkHours) {
+  workHours.append("\(hours.from)-\(hours.to)")
+}
+
+func addClosed() {
+  workHours.append("Closed")
+}
+
+displayShopWorkSchedule(shop: shop1)
+print("")
+print("")
+displayShopWorkSchedule(shop: shop2)
+print("")
+print("")
+displayShopWorkSchedule(shop: shop3)
+
+
 
 /****
  2. Write a function to find the longest common prefix from string array (function should take string as parameter and return a string).
@@ -7,9 +136,18 @@ import Foundation
  If there is no common prefix return an empty string.
  ***/
 
+//Idea from https://stackoverflow.com/questions/48449365/how-to-determine-longest-common-prefix-and-suffix-for-array-of-strings?rq=1
+//using Foundation library
+
 func longestCommonPrefix(words: [String]) -> String {
-  return ""
+  guard let first = words.first.map({ String($0) }) else { return "" }
+  return words.dropFirst().reduce(first, { $0.commonPrefix(with: $1) })
 }
+
+longestCommonPrefix(words: ["market", "maxima", "mama"])
+longestCommonPrefix(words: ["sale", "safe", "sun"])
+
+
 
 
 /****
@@ -33,7 +171,7 @@ func lastWordsLength(sentence: String) -> Int {
   return 0
 }
 
-print(lastWordsLength(sentence: "Today is sunny")) //Testing lastWordsLength function
+lastWordsLength(sentence: "Today is sunny")
 
 
 
@@ -63,7 +201,7 @@ func filterArray(numbers: [Int]) -> [Int] {
   return filteredArray
 }
 
-print(filterArray(numbers: [1,1,3,4,5,5]))
+filterArray(numbers: [1,1,3,4,5,5])
 
 
 
@@ -75,7 +213,7 @@ B -> 2
 C -> 3
 Z -> 26
 AA -> 27
-AB-> 28
+AB -> 28
 ****/
 
 //Idea from https://www.geeksforgeeks.org/find-excel-column-number-column-title/?ref=lbp
@@ -88,7 +226,7 @@ func getExcelColumnNumber(column: String) -> Int {
   return result
 }
 
-print(getExcelColumnNumber(column: "AB"))
+getExcelColumnNumber(column: "AB")
 
 
 
@@ -105,14 +243,57 @@ D 500
 M 1000
  ***/
 
-/**
-7. Calculate smallest time difference. Function should take 2 strings in “hh:mm” format (hours:minutes) and return a difference in minutes.
+//Idea from https://learnappmaking.com/roman-numerals-swift/
+func getRomanNumberFromNumber(number: Int) -> String {
+  let romanValues = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+  let arabicValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+  var romanValue = ""
+  var startingValue = number
+  
+  for (index, romanChar) in romanValues.enumerated() {
+    let arabicValue = arabicValues[index]
+    let div = startingValue / arabicValue
+  
+    if (div > 0) {
+      for _ in 0..<div {
+        romanValue += romanChar
+      }
+      startingValue -= arabicValue * div
+    }
+  }
+  return romanValue
+}
+
+getRomanNumberFromNumber(number: 2000)
+
+
+
+
+ /**
+ 7. Calculate smallest time difference. Function should take 2 strings in “hh:mm” format (hours:minutes) and return a difference in minutes.
  For example:
-Input - [“13:45”, ”12:45”], output 60
-Input - [“13:45”, ”14:00”], output 15
- ***/
+ Input - [“13:45”, ”12:45”], output 60
+ Input - [“13:45”, ”14:00”], output 15
+  ***/
 
+func smallestTimeDifference(time: [String]) -> Int {
+  let formatter = DateFormatter()
+  formatter.dateFormat = "HH:mm"
 
+  let time1 = formatter.date(from: time[0])!
+  let time2 = formatter.date(from: time[1])!
+
+  let elapsedTime = time2.timeIntervalSince(time1)
+  
+  let hours = floor(elapsedTime / 60)
+  let minutes = floor((elapsedTime - (hours * 60)) / 60)
+  
+  return abs(Int(minutes)+Int(hours))
+}
+
+smallestTimeDifference(time: ["13:45", "12:45"])
+smallestTimeDifference(time: ["13:45", "14:00"])
 
 
 
@@ -131,3 +312,27 @@ Input - [6, 3, 3, 6, 2, 9, 1], output - 10
 This time you should buy stock on 2nd day for 3, sell it on 5th day for 6, buy it again
 on 6th day for 2 and sell it next day for 9. Thus total profit would be: 6-3+9-2 = 10
  ***/
+
+func investmentAlgorithm(stockPrices: [Int]) -> Int {
+  var profit = 0
+  var needToBuy = true
+  
+  for (index, price) in stockPrices.enumerated() {
+    if needToBuy {
+      if index+1 < stockPrices.count, stockPrices[index] < stockPrices[index+1] {
+        profit -= price
+        needToBuy = false
+      }
+    } else {
+      if (index+1 < stockPrices.count && stockPrices[index] > stockPrices[index+1]) || index+1 == stockPrices.count {
+        profit += price
+        needToBuy = true
+      }
+    }
+  }
+  
+  return profit
+}
+
+investmentAlgorithm(stockPrices: [1, 4, 5, 6, 2, 8])
+investmentAlgorithm(stockPrices: [6, 3, 3, 6, 2, 9, 1])
